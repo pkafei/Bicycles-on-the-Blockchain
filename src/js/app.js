887f4db5d05f1380ade77b7fe05e8b3ff4887fcf
Portia Burton <plburton@gmail.com>
2018-03-10 15:01:48 -0500

    src/index.html

--- src/js/app.js
+++ src/js/app.js
@@ -9,11 +9,11 @@ App = {
       var petTemplate = $('#petTemplate');
 
       for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
+        petTemplate.find('.panel-title').text(data[i].location);
         petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
+        petTemplate.find('.pet-breed').text(data[i].brand);
         petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
+        petTemplate.find('.pet-location').text(data[i].value);
         petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
 
         petsRow.append(petTemplate.html());
